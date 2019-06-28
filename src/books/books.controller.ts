969e91b36465445ae64ca7ecbb737512fed229b0
hadi rasouli <rasuli.hadi4@gmail.com>
2019-06-28 21:59:53 +0430

    update book  swagger

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,10 +1,18 @@
 import { Controller, Get, Param, Post, Body, Query, Delete } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { CreateBookDTO } from './dto/create-book.dto';
+import { ApiBearerAuth, ApiUseTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+
+
+@ApiBearerAuth()
+@ApiUseTags('books')
 @Controller('books')
 export class BooksController {
     constructor(private booksService: BooksService) { }
 
+
+    @ApiOperation({title: 'Get all books'})
+    @ApiResponse({status: 200, description: 'Return all books.'})
     @Get()
     async getBooks() {
         const books = await this.booksService.getBooks();
